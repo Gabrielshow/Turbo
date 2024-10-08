ebd0cb0d2c585fd5f0d50eb241ada4ab8c820270
= <=>
2024-10-08 03:34:00 +0100

    installing libraries

--- src/components/EmailList.vue
+++ src/components/EmailList.vue
@@ -1,34 +1,35 @@
 <template>
-
-    <div>
-        <h1>Inbox</h1>
-        <ul>
-            <li v-for="email in emails" :key="email.id">
-                {{ email.subject }}
-            </li>
-        </ul>
-    </div>
-
-
+  <div>
+    <h1>Inbox</h1>
+    <ul>
+      <li v-for="email in emails" :key="email.id">
+        {{ email.subject }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-    data() {
-        return {
-            emails: [
-                {
-                    id: 1, subject: "Email 1"
-                },
-                { id: 2, subject: "Email 2" },
-                {
-                    id: 3, subject: "Email 3"
-                },
-            ],
-        };
+  data() {
+    return {
+      emails: [],
+    };
+  },
+  mounted() {
+    this.getEmails();
+  },
+  methods: {
+    getEmails() {
+      axios.get('http://localhost:3000/email')
+        .then((response) => {
+          this.emails = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
+  },
 };
-
 </script>
-
-<style></style>
