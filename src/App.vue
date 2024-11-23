ba0baabc6fbbe3db7bd28d55e35d6588e7748083
Gabrielshow <adefelashogbanmu@gmail.com>
2024-11-23 01:04:38 +0100

    Made changes to Navbar - auto push

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="main">
-    <AppLogo />
-    <!-- <AppNavbar /> -->
+    <AppNavbar />
   </div>
   <Sidebar @show-modal="showModal = true" />
   <router-view />
@@ -9,25 +8,20 @@
 </template>
 
 <script setup>
-// import AppNavbar from '@/components/AppNavbar.vue'
-import AppLogo from '@/components/AppLogo.vue'
-import Sidebar from '@/components/SideBar.vue'
+import AppNavbar from '@/components/AppNavbar.vue'
+// import AppLogo from '@/components/AppLogo.vue'
+import Sidebar from '@/components/Sidebar.vue'
 import ComposeEmail from '@/components/ComposeEmail.vue'
 
 import { ref } from 'vue'
 
 const showModal = ref(false)
+
 </script>
 
 <style scoped>
 .main {
-  margin: 0px 0px;
-  padding: 0px 4px;
   width: 100vw;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
   background: linear-gradient(to right, white, grey);
   height: 80px;
   /* border-left: 5px solid Purple; */
--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,21 +1,73 @@
 <template>
-    <div class="group">
-        <input placeholder="Search mail" v-model="query"/>
-        <button> Signup </button>
-        <button class="login"> Login
-        </button>
+    <div className="navbar">
+        <div className="logo-container">
+            <button className="menu" @click="handleSidebar"> place</button>
+            <AppLogo />
+        </div>
+        <div class="group">
+            <input placeholder="Search mail" v-model="query"/>
+            <button> Signup </button>
+            <button class="login"> Login
+            </button>
+        </div>
     </div>
 
 </template>
 
 <script>
-// import { ref } from 'vue';
-// const query = ref("");
+
+import AppLogo from '@/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  },
+  emit: 'close-sidebar',
+  methods: {
+        closeSidebar() {
+            this.$emit('close-sidebar');
+        }
+},
+    data() {
+        return {
+            query: '',
+            isSidebarOpen: true,
+        };
+        }
+}
 
 </script>
 
 
 <style scoped>
+.navbar {
+    width: 100vw;
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    gap: 40px;
+    justify-content: space-between;
+    background-color: #f0f0f0;
+    border-bottom: 1px solid #ccc;
+}
+
+.logo-container {
+    display: flex;
+    flex-direction: row;
+    gap: 16px;
+    justify-content: space-between;
+}
+
+.menu {
+    border-radius: 50%;
+    height: 50px;
+    width: 50px;
+    cursor: pointer;
+
+}
+
 .group {
     display: flex;
     justify-content: space-between;
